b09ba265dd179523baece803a3baf33751939ca3
Johnny <km.monzurul.islam@gmail.com>
2021-11-20 22:36:28 -0500

    settings update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-dj1p9iiyrn^o43!gqi$e^znbb383=pgn=o_7*@*%$uht)#ode0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://my-web-portfolio-v1.herokuapp.com/',
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
